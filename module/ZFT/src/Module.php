fb557404dc444f55f43cdf928b99778c79774df2
Hubko_VB <hubertkotkom@gmail.com>
2017-03-05 23:27:02 +0100

    Implementacje ServiceProvider w module ZFT oraz Implementacje ConfigInterface w module POrtal

--- module/ZFT/src/Module.php
+++ module/ZFT/src/Module.php
@@ -6,6 +6,11 @@
  */
 namespace ZFT;
 
-class Module {
+use Zend\ModuleManager\Feature\ServiceProviderInterface;
+
+class Module implements ServiceProviderInterface {
+    public function getServiceConfig() {
+        return[];
+    }
 
 }
