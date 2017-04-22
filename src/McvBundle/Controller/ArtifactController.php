ea52ede748e62bdf87c2db82240bdf7306ad1113
Hubko_VB <hubertkotkom@gmail.com>
2017-04-23 01:14:41 +0200

    Add ArtifactFixtures Add DescriptionAction() to show single artifact row.

--- src/McvBundle/Controller/ArtifactController.php
+++ src/McvBundle/Controller/ArtifactController.php
@@ -19,10 +19,27 @@ class ArtifactController extends Controller{
      * @Route("/artifact/list", name="artifact_list")
      */
     public function showListAction(){
-        #$em = $this->getDoctrine()->getManager();
-        #$artifact =$this->getDoctrine()->getRepository('McvBundle:Artifact');
-        #$artifact->findAll();
-        #echo '<pre>', print_r($artifact),'</pre>';
-        return $this->render('McvBundle:mcv:artifact/basic.html.twig');
+        
+        $artifact = $this->getDoctrine()
+                ->getRepository('McvBundle:Artifact')
+                ->findAll();
+        if(!$artifact){
+            throw $this->createNotFoundException(
+                    'Nie znalazłem żadnego rekordu artifact');
+        }
+        return $this->render('McvBundle:mcv:artifact/list.html.twig', ['artifact' => $artifact]);
     }
+    /**
+     * @Route("/artifact/desc/{inventoryNumber}", name="artifact_description")
+     * @param string $inventoryNumber
+     */
+    public function descriptionAction($inventoryNumber){
+        
+        $artifact = $this->getDoctrine()
+                ->getRepository('McvBundle:Artifact')
+                ->findOneBy(['inventoryNumber' => $inventoryNumber]);
+        print_r($artifact);
+        return $this->render('McvBundle:mcv:artifact/description.html.twig', ['artifact' => $artifact]);
+    }
+    
 }
