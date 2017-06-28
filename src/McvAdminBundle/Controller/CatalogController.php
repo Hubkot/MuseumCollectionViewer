45c9edc0b7dc8ca0fe6eee0481c638c2e086a65c
Hubko_VB <hubertkotkom@gmail.com>
2017-06-28 18:25:23 +0200

    Create scan button in McvAdminBundle::finderAction(). Scan will proceed after submit.

--- src/McvAdminBundle/Controller/CatalogController.php
+++ src/McvAdminBundle/Controller/CatalogController.php
@@ -5,11 +5,13 @@ namespace McvAdminBundle\Controller;
 use McvAdminBundle\Service\CollectionFinder;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 
 class CatalogController extends Controller
 {
      /**
-     * @Route("/catalog")
+     * @Route("/catalog", name="catalog")
      */
     public function indexAction()
     {
@@ -18,11 +20,19 @@ class CatalogController extends Controller
      /**
      * @Route("/collection-finder", name="collection-finder")
      */
-    public function finderAction()
+    public function finderAction(Request $request)
     {
-        $findFiles = new CollectionFinder($this->getParameter('upload_dir'));
-        $findFiles->findAll();
-     
-        return $this->render('McvAdminBundle:catalog:list.catalog.html.twig', ['findFiles' => $findFiles]);
+        //Tworzenie przycisku POST do rozpoczęcia skanowania plików
+        $scan_button = $this->createFormBuilder()->add('submit', SubmitType::class,array('label'=>'Skanuj'))->getForm();
+        $scan_button->handleRequest($request);
+        
+        if($scan_button->isSubmitted()){
+            $findFiles = new CollectionFinder($this->getParameter('upload_dir'));
+            $findFiles->findAll();
+            return $this->render('McvAdminBundle:catalog:scan.catalog.html.twig', ['findFiles' => $findFiles,'scan_button'=>$scan_button->createView()]);
+        }
+
+        return $this->render('McvAdminBundle:catalog:list.catalog.html.twig', ['scan_button'=>$scan_button->createView()]);
+        
     }
 }
